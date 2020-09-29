cd74210236e3607c790954382634e852d8abb28b
xames3 <xames3.developer@gmail.com>
2020-09-29 21:41:47 +0530

    Add examples

---
+++ more_than_basics/methods/strings.py
@@ -0,0 +1,5 @@
+xa = "I'm XAMES3 and I can do whatever dafaq I want!"
+
+string = "{name} is good at using {skill_1} and {skill_3}"
+
+print(string.format(skill_1="GitHub", name="Srushti", skill_3="Cooking"))

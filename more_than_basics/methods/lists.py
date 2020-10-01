79ad7431f7e4ce063add11a2e647de56160e2bad
xames3 <xames3.developer@gmail.com>
2020-10-01 21:40:42 +0530

    Add dummy list method module

---
+++ more_than_basics/methods/lists.py
@@ -0,0 +1,15 @@
+# import this
+
+# print(this)
+
+# xa = [idx for idx in range(10) for jdx in range(idx)]
+
+# xa = []
+# for idx in range(10):
+#   xa.append(idx)
+
+try:
+  5 / 0
+except ZeroDivisionError:
+  print("Naahi chaalat")
+# print("Done!")

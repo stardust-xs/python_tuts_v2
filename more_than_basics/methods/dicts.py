aa3250065b46fbf99c60ba08a3e425d198ca569a
xames3 <xames3.developer@gmail.com>
2020-10-04 00:33:24 +0530

    Update dict methods

--- more_than_basics/methods/dicts.py
+++ more_than_basics/methods/dicts.py
@@ -1,6 +1,6 @@
 xa = {"name": "XA", "age": 25, "type": "God-tier"}
 za = {"status": "Single", "Profession": "Developer", "Sign": "Gemini"}
 
-ya = xa.update(za)
+ya = xa.clear()
 
 print("XA:\n", xa, "\nYA:\n", ya)

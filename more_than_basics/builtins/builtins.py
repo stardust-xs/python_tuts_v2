3a27abc02ceeb81277495346d6cdb09457adc91c
xames3 <xames3.developer@gmail.com>
2020-10-08 22:05:41 +0530

    Update builtins

--- more_than_basics/builtins/builtins.py
+++ more_than_basics/builtins/builtins.py
@@ -1,1 +1,13 @@
 # List of Builtins referred from here: https://docs.python.org/3/library/functions.html
+
+import sys
+xa = "XA"
+xa = open("/mnt/storage/development/programming/text.txt", "w")
+print("xa is a nice person")
+
+# xa = ["xa", "pranali", 'nimesh', "shailesh", "srushti", 56, 59]
+# ya = []
+# for idx in xa:
+#   if isinstance(idx, str):
+#     ya.append(idx.upper())
+# print(ya)

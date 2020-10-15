8777672d9793110abc0a6dc80e58687cbf0ce4a4
xames3 <xames3.developer@gmail.com>
2020-10-16 00:57:27 +0530

    Update comprehensions

--- more_than_basics/comprehensions/comprehensions.py
+++ more_than_basics/comprehensions/comprehensions.py
@@ -2,25 +2,48 @@
 # Accessing the file ending with .pem extension.
 # Rename.
 
+# import glob
+# from os.path import join
+# from kaamiki import socket
+
 import os
 
-path = "/mnt/storage/extracted/impactor_test/"
-# xa = os.listdir(path)
-# os.rename(f'{path}/xa2.pem', f"{path}/xa3.pem")
+# "/mnt/storage/development/programming/xa.py" -> Absolute Path/Complete Path
+# "xa.py" -> Basename
+# "xa" -> Stem
 
-for file in os.listdir(path):
-#   print(file, type(file))
-  if file.endswith(".so"):
-    source = os.path.join(path, file)
-    dest = os.path.join(path, "XA.xa")
-    print(source, dest)
+path = "/mnt/storage/development/programming/beta/python_tutorials/exercises/basics/atm_emulator/"
+
+# xa = os.listdir(f"{path}")
+
+# src = xa[3]
+# dst = xa[3].replace("__init__", "xa")
+
+# src = path + src
+# dst = path + dst
+
+# os.rename(src, dst)
+
+for obj in os.listdir(path):
+  if "__init__.py" == obj:
+    src = obj
+    dst = obj.replace("__init__", "xa-is-god")
+    src = path + src
+    dst = path + dst
+    os.rename(src, dst)
+
+  #   source = os.path.join(path, file)
+  #   dest = os.path.join(path, "XA.xa")
+  #   print(source, dest)
 
     # os.rename(f"{path}{file}", f"{path}XA.xa")
 
-# print(dir(str))
+# print(dir(str.replace()))
 
-
-
+# a = 10
+# b = 10
+# c = a
+# print(a is b)
 
 
 

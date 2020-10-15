745a0e229d79748c0afc5f27ad712d3b54c8804e
xames3 <xames3.developer@gmail.com>
2020-10-15 22:02:02 +0530

    Update builtins with test data

--- more_than_basics/builtins/builtins.py
+++ more_than_basics/builtins/builtins.py
@@ -1,5 +1,22 @@
 # List of Builtins referred from here: https://docs.python.org/3/library/functions.html
 
-xa = oct(10)
+# xa = oct(10)
 
-print(xa, (type(xa)), sep="\n")
+import os
+import glob
+
+# print(xa, (type(xa)), sep="\n")
+
+xa = ["xa", "pranali", "srushti", "shailesh", "nimesh"]
+xa = [idx.upper() for idx in xa]
+# ya = xa.copy()
+# za = []
+
+# for idx in xa:
+#   za.append(idx.upper())
+
+print(xa)
+# print(za)
+
+# 1. Write brackets
+# 2. Write for loop as is BUT w/o :

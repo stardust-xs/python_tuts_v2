86cf31cc5ba9d7d1314af10ec4dce7b94b3a0d3d
xames3 <xames3.developer@gmail.com>
2020-10-20 23:20:41 +0530

    Update comprehensions

--- more_than_basics/comprehensions/comprehensions.py
+++ more_than_basics/comprehensions/comprehensions.py
@@ -1,61 +1,29 @@
-# Navigated to that file.
-# Accessing the file ending with .pem extension.
-# Rename.
+videos = ["video_1.mp4",
+          "video_2.mp4",
+          "video_3.mp4",
+          "video_4.mp4"]
 
-# import glob
-# from os.path import join
-# from kaamiki import socket
+links = ["https://xa.com/video_1.mp4",
+         "https://xa.com/video_2.mp4",
+         "https://xa.com/video_3.mp4",
+         "https://xa.com/video_4.mp4"]
 
-import os
+# output = [
+#   {1: {"name": "video_1.mp4", "link": "https://xa.com/video_1.mp4"}},
+#   {2: {"name": "video_2.mp4", "link": "https://xa.com/video_2.mp4"}},
+#   {3: {"name": "video_3.mp4", "link": "https://xa.com/video_3.mp4"}},
+#   {4: {"name": "video_4.mp4", "link": "https://xa.com/video_4.mp4"}},
+#   ]
 
-# "/mnt/storage/development/programming/xa.py" -> Absolute Path/Complete Path
-# "xa.py" -> Basename
-# "xa" -> Stem
+xa = []
+for idx in zip(videos, links):
+  xa.append(idx)
 
-path = "/mnt/storage/development/programming/beta/python_tutorials/exercises/basics/atm_emulator/"
+za = {}
+ya = []
+for idx in xa:
+  ya.append({"name": idx[0], "link": idx[1]})
 
-# xa = os.listdir(f"{path}")
-
-# src = xa[3]
-# dst = xa[3].replace("__init__", "xa")
-
-# src = path + src
-# dst = path + dst
-
-# os.rename(src, dst)
-
-for obj in os.listdir(path):
-  if "__init__.py" == obj:
-    src = obj
-    dst = obj.replace("__init__", "xa-is-god")
-    src = path + src
-    dst = path + dst
-    os.rename(src, dst)
-
-  #   source = os.path.join(path, file)
-  #   dest = os.path.join(path, "XA.xa")
-  #   print(source, dest)
-
-    # os.rename(f"{path}{file}", f"{path}XA.xa")
-
-# print(dir(str.replace()))
-
-# a = 10
-# b = 10
-# c = a
-# print(a is b)
-
-
-
-
-# isinstance
-
-
-# xa = ["xa", "pranali", "srushti", "shailesh", "nimesh"]
-# xa = [idx.upper() for idx in xa]
-# ya = xa.copy()
-# za = []
-# for idx in xa:
-#   (idx.upper())
-# print(xa)
-# print(za)
+print(ya)
+# za.setdefault() -> Adds key-value pair but doesnt update new values
+# za.update() -> Adds key-value pair but references to the latest values

018f9d9d4eb63dc8939c51a83792a47e04338092
xames3 <xames3.developer@gmail.com>
2020-10-23 21:50:44 +0530

    Add inline if-else

---
+++ more_than_basics/if-else/if-else.py
@@ -0,0 +1,72 @@
+# if condition:
+#   pass
+# else:
+#   pass
+
+# if condition:
+#   pass
+# elif expression:
+#   pass
+# else:
+#   pass
+
+# if condition:
+#   pass
+
+# pass if condition else pass
+
+xa = "XA"
+
+# if xa == "XA":
+#   print("Yo!")
+# else:
+#   print("No!")
+
+# print("Yo!") if xa == "XA" else print("No!")
+
+# print("Yo!") if xa == "XA" else print("No!")
+
+# if condition:
+#   pass
+# else:
+#   if expression:
+#     pass
+# else:
+#   pass
+
+# pass if condition else pass if expression else pass
+xa = "XA"
+
+# if xa == "XA":
+#   print("Yo!")
+# elif xa == "SK":
+#   print("So!")
+# elif xa == "NM":
+#   print("No!")
+# else:
+#   print("Po!")
+
+# print("Yo!") if xa == "XA" else print("So!") if xa == "SK" else print("No!") if xa == "NM" else print("Po!")
+
+# print("Yo!") if xa == "XA" else print("So!") if xa == "SK" else print("No!") if xa == "NM" else print("Po!")
+
+
+# if condition:
+#   pass
+
+# if xa == "XXXXXX":
+#   print("Yo!")
+
+
+# if xa == "XA":print("Yo!")
+# for idx in range(10):print(idx)
+# while True:print()
+
+# xa = 0
+# while xa < 10:
+#   print(xa)
+#   xa += 1
+#   print("XA")
+
+xa = 0
+while xa < 10:print(xa);xa+=1;print("XA")
